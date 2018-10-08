7b2f674d38ee9adbf8364e5d101ab1f71ba9c8a8
FabioPeart <fabiopeart@gmail.com>
2018-10-08 15:58:58 -0500

    Added controllers and views for each model. Also made views working along with its controller

--- app/controllers/teamcolors_controller.rb
+++ app/controllers/teamcolors_controller.rb
@@ -1,7 +1,9 @@
 class TeamcolorsController < ApplicationController
   def index
+    @teamcolors = Teamcolor.all
   end
 
   def show
+     @teamcolor = Teamcolor.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,28 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'teamcolors/index'
-  get 'teamcolors/show'
-  get 'cards/index'
-  get 'cards/show'
+
+  root to: 'users#index'
+
+  resources :users, only: [:index, :show]
+  resources :teamcolors, only: [:index, :show]
+  resources :cards, only: [:index, :show]
+
+  #get 'pages/about', to: 'pages#about'
+  get 'about', to: 'pages#about', as: 'about'
+  # get 'teamcolor', to: 'teamcolors#index', as: 'teamcolor'
+  # get 'card', to: 'cards#index', as: 'card'
+
+
+  # get 'users/index'
+  # get 'users' => 'users#index'
+ 
+
+  # get 'users/show'
+  # get 'users/:id', to: 'users#show', id: /\d+/
+
+
+  # get 'teamcolors/index'
+  # get 'teamcolors/show'
+  # get 'cards/index'
+  # get 'cards/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
